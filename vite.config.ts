856b782554c09bcf2a1ab7839e1b8d33749a00e1
Dean Quinney <slpixe@gmail.com>
2026-07-03 02:25:46 +0100

    chore: rename app title to Sudoku

--- vite.config.ts
+++ vite.config.ts
@@ -27,8 +27,8 @@ export default defineConfig(() => ({
         ],
       },
       manifest: {
-        name: "Super Sudoku",
-        short_name: "super-sudoku",
+        name: "Sudoku",
+        short_name: "Sudoku",
         description: "Play over 500 Sudoku puzzles ranging from easy to evil",
         theme_color: "#1F2937",
         background_color: "#1F2937",
